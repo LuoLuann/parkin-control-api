54f9d15a9ecfdc7b12a3b1d034601250a7640ce4
LuoLuann <luo.luann@gmail.com>
2023-08-15 03:44:11 -0300

    spring security funcionando para salvamento em memoria

--- src/main/java/com/api/parkingcontrol/configs/security/WebSecurityConfig.java
+++ src/main/java/com/api/parkingcontrol/configs/security/WebSecurityConfig.java
@@ -1,8 +1,12 @@
 package com.api.parkingcontrol.configs.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 // forma antiga de implementar o security
 
@@ -33,5 +37,24 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated()
                 .and()
                 .csrf().disable();
+    }
+
+    // vamos utilizar uma autentificação por memoria para fins de aprendizagem
+    @Override
+    protected  void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        // precisa colocar pelo menos uma role, mas pode ter várias
+        // o spring security olha um password encoder, não uma senha comum
+        auth.inMemoryAuthentication()
+                .withUser("luann")
+                .password(passwordEncoder().encode("12345678"))
+                .roles("ADMIN");
+    }
+
+    // essa função serve para encoder a senha, para que então o spring security
+    // valide ela, eh um bean do próprio spring security
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
